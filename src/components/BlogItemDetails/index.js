7d40dccbe4566e1b2a84ee817c73bc25d803b25f
Sai Vara Prasad Degala <93783719+Saivaraprasad48@users.noreply.github.com>
2023-03-29 20:13:46 +0530

    Id and test-case Id should be correct
    https://apis.ccbp.in/blogs/
    https://apis.ccbp.in/blogs/${id}
    Need to render the blog based on "Id"!
    
    data-testid="loader"
    testid="loader"
    As per test-case use only "testid" instead of "data-testid"

--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -16,7 +16,7 @@ class BlogItemDetails extends Component {
     const {params} = match
     const {id} = params
 
-    const response = await fetch(`https://apis.ccbp.in/blogs`)
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
     const data = await response.json()
     const updatedData = {
       title: data.title,
@@ -52,7 +52,7 @@ class BlogItemDetails extends Component {
     return (
       <div className="blog-container">
         {isLoading ? (
-          <div data-testid="loader">
+          <div testid="loader">
             <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
           </div>
         ) : (
